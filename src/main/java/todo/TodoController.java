315fea0bfe7367148e318b9d6734b84e58d9f2de
naokiur <atomic.transfer.comp@gmail.com>
2017-05-09 01:18:57 +0900

    Add Injection.

--- src/main/java/todo/TodoController.java
+++ src/main/java/todo/TodoController.java
@@ -1,15 +1,20 @@
 package todo;
 
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import todo.domain.model.Todo;
 import todo.domain.service.todo.TodoService;
 
 @Controller
 public class TodoController {
 
+    @Autowired
     TodoService todoService;
 
     @ModelAttribute
@@ -21,8 +26,8 @@ public class TodoController {
     @RequestMapping(value = "list")
     public String list(Model model) {
         // TODO Injection
-//        Collection<Todo> todos = todoService.findAll();
-//        model.addAttribute("todos", todos);
+        Collection<Todo> todos = todoService.findAll();
+        model.addAttribute("todos", todos);
 
         return "todo/list";
     }
--- src/main/java/todo/domain/service/todo/TodoServiceImpl.java
+++ src/main/java/todo/domain/service/todo/TodoServiceImpl.java
@@ -18,7 +18,12 @@ public class TodoServiceImpl implements TodoService {
 
     // Because this repository has only 1 constructor, Annotation is not need ?
     // https://docs.spring.io/spring-boot/docs/current/reference/html/using-boot-spring-beans-and-dependency-injection.html
+    // -> This is wrong. If Developer does not want to write annotation, Developer need to write constructor.
     TodoRepository todoRepository;
+
+    public TodoServiceImpl(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
 
     @Override
     public Todo findOne(String todoId) {
