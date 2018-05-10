4457476c57508d1ad092fb4d46a941e138092611
naokiur <atomic.transfer.comp@gmail.com>
2018-05-10 09:02:48 +0900

    [feat]Initial commit.

---
+++ android/app/src/main/java/jp/ne/naokiur/todomanagement/MainActivity.kt
@@ -0,0 +1,12 @@
+package jp.ne.naokiur.todomanagement
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
